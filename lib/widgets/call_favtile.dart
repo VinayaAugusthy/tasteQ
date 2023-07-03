9dfbe69b7a1bb5af23f6f81ddd9254ced0cb1804
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-07-03 14:44:21 +0530

    favourite tile widget created

---
+++ lib/widgets/call_favtile.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:tasteq/model/recipe/recipe.dart';
+
+Widget callTile(Recipe recipe, BuildContext context, int index) {
+  return ListTile(
+    leading: CircleAvatar(
+      radius: 25,
+      backgroundImage: FileImage(File(recipe.image)),
+    ),
+    title: Text(recipe.name),
+    subtitle: Text(recipe.category),
+    trailing: IconButton(
+      onPressed: () {
+        // delFavourite(recipe.name);
+        // getFavourite();
+      },
+      icon: const Icon(Icons.remove_circle_outline_rounded),
+      color: Colors.red,
+    ),
+    onTap: () {
+      // Navigator.push(
+      //   context,
+      //   MaterialPageRoute(
+      //     builder: (context) => ViewRecipes(passValue: recipe, passId: index),
+      //   ),
+      // );
+    },
+  );
+}
